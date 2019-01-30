7bf90c174dfc2db9a80a2939f463af73358ff236
weizhonzhen <czdswzz@126.com>
2019-01-30 14:48:41 +0800

    Update Data_MapFile.cs

--- FastData.Core/DataModel/SqlServer/Data_MapFile.cs
+++ FastData.Core/DataModel/SqlServer/Data_MapFile.cs
@@ -1,4 +1,4 @@
-﻿using FastData.Core.Property;
+using FastData.Core.Property;
 using System;
 
 namespace FastData.Core.DataModel.SqlServer
@@ -12,19 +12,19 @@ namespace FastData.Core.DataModel.SqlServer
         /// <summary>
         /// map id
         /// </summary>
-        [Column(Comments = "Map id", DataType = "Char", Length = 32, IsNull = false, IsKey = true)]
+        [Column(Comments = "Map id", DataType = "varchar", Length = 32, IsNull = false, IsKey = true)]
         public string MapId { get; set; }
 
         /// <summary>
         /// 文件名称
         /// </summary>
-        [Column(Comments = "文件名称", DataType = "Char", IsNull = true, Length = 32)]
+        [Column(Comments = "文件名称", DataType = "varchar", IsNull = true, Length = 32)]
         public string FileName { get; set; }
 
         /// <summary>
         /// 文件路径
         /// </summary>
-        [Column(Comments = "文件路径", DataType = "Char", IsNull = true, Length = 256)]
+        [Column(Comments = "文件路径", DataType = "varchar", IsNull = true, Length = 256)]
         public string FilePath { get; set; }
 
         /// <summary>
