462b5b9b9beb1995c307215c879d10ba99aad1fe
weizhonzhen <czdswzz@126.com>
2019-08-18 15:49:58 +0800

    Update DataConfig.cs

--- FastData.Core/Base/DataConfig.cs
+++ FastData.Core/Base/DataConfig.cs
@@ -1,9 +1,10 @@
 using FastData.Core.Model;
 using FastUntility.Core.Base;
+using System.Collections.Generic;
 
 namespace FastData.Core.Base
 {
-    internal static class DataConfig
+    public static class DataConfig
     {
         /// <summary>
         /// 获取配置实体
@@ -12,11 +13,22 @@ namespace FastData.Core.Base
         /// <returns></returns>
         public static ConfigModel Get(string key=null)
         {
+            var list = new List<ConfigModel>();
             var item = new ConfigModel();
-            if (key != null)
-                item = BaseConfig.GetListValue<ConfigModel>(AppSettingKey.Config,"db.json").Find(a => a.Key.ToLower() == key.ToLower());
+            var cacheKey = key == null ? "config" : string.Format("config.{0}", key);
+
+            if (DbCache.Exists(CacheType.Web, cacheKey))
+                list = DbCache.Get<List<ConfigModel>>(CacheType.Web,cacheKey);
             else
-                item = BaseConfig.GetListValue<ConfigModel>(AppSettingKey.Config,"db.json")[0] ?? new ConfigModel();
+            {
+                list = BaseConfig.GetListValue<ConfigModel>(AppSettingKey.Config, "db.json");
+                DbCache.Set<List<ConfigModel>>(CacheType.Web, cacheKey, list);
+            }
+
+            if (key == null)
+                item = list[0];
+            else
+                item = list.Find(a => a.Key.ToLower() == key.ToLower());
 
             if (item.DesignModel == "")
                 item.DesignModel = Config.DbFirst;
