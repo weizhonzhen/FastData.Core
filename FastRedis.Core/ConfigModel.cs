2106c9f5a520b6939dc16dade634cdad28ed8ff3
weizhonzhen <czdswzz@126.com>
2019-12-29 00:00:12 +0800

    Update ConfigModel.cs

--- FastRedis.Core/ConfigModel.cs
+++ FastRedis.Core/ConfigModel.cs
@@ -1,4 +1,4 @@
-﻿namespace FastRedis.Core
+namespace FastRedis.Core
 {
     /// <summary>
     /// 配置实体
@@ -6,28 +6,8 @@
     internal class ConfigModel
     {
         /// <summary>
-        /// 写服务器,各个服务器之间用逗号分开
+        /// 服务器
         /// </summary>
-        public string WriteServerList { get; set; }
-
-        /// <summary>
-        ///读服务器,各个服务器之间用逗号分开
-        /// </summary>
-        public string ReadServerList { get; set; }
-
-        /// <summary>
-        /// 最大写链接数
-        /// </summary>
-        public int MaxWritePoolSize { get; set; }
-
-        /// <summary>
-        /// 最大读链接数
-        /// </summary>
-        public int MaxReadPoolSize { get; set; }
-
-        /// <summary>
-        /// 自动重启
-        /// </summary>
-        public bool AutoStart { get; set; }        
+        public string Server { get; set; }       
     }
 }
