ee66d215ae3ecbf605d843d387608d336d6a23d8
weizhonzhen <czdswzz@126.com>
2020-09-01 11:54:25 +0800

    Update ConfigModel.cs

--- FastData.Core/Model/ConfigModel.cs
+++ FastData.Core/Model/ConfigModel.cs
@@ -84,5 +84,10 @@
         /// 是否更新缓存
         /// </summary>
         public bool IsUpdateCache { get; set; }
+                
+        /// <summary>
+        /// 切换数据库
+        /// </summary>
+        public bool IsChangeDb { get; set; }
     }
 }
