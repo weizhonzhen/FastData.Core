0c1dfe64594a6ffd9ccd6ac901bc63be88d2990b
weizhonzhen <czdswzz@126.com>
2020-10-19 16:36:50 +0800

    Update BaseLog.cs

--- FastUntility.Core/Base/BaseLog.cs
+++ FastUntility.Core/Base/BaseLog.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
@@ -35,14 +35,15 @@ namespace FastUntility.Core.Base
                     //写日志
                     using (var fs = new FileStream(string.Format("{0}/{1}", path, fileName), FileMode.OpenOrCreate, FileAccess.Write))
                     {
-                        var m_streamWriter = new StreamWriter(fs);
-                        m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
-                        m_streamWriter.WriteLine(string.Format("[{0}]{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logContent));
-                        m_streamWriter.WriteLine("");
-
-                        m_streamWriter.Flush();
-                        m_streamWriter.Close();
-                        fs.Close();
+                        using (var m_streamWriter = new StreamWriter(fs))
+                        {
+                            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+                            m_streamWriter.WriteLine(string.Format("[{0}]{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logContent));
+                            m_streamWriter.WriteLine("");
+                            m_streamWriter.Flush();
+                            m_streamWriter.Close();
+                            fs.Close();
+                        }
                     }
                 }
                 finally
