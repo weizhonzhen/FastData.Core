525599480aae4d786cc5cdd223a2248c98ec2ea6
weizhonzhen <czdswzz@126.com>
2020-10-21 10:30:22 +0800

    Add files via upload

--- FastUntility.Core/Base/BaseLog.cs
+++ FastUntility.Core/Base/BaseLog.cs
@@ -1,7 +1,6 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using System.IO;
-using System.Threading;
 
 namespace FastUntility.Core.Base
 {
@@ -15,40 +14,32 @@ namespace FastUntility.Core.Base
         /// <param name="StrContent">日志内容</param>
         public static void SaveLog(string logContent, string fileName)
         {
-            using (var lockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion))
+            var path = string.Format("{0}/App_Data/log/{1}/{2}", AppDomain.CurrentDomain.BaseDirectory, DateTime.Now.ToString("yyyy-MM"), DateTime.Now.ToString("yyyy-MM-dd"));
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            if (fileName == "")
+                fileName = string.Format("{0}.txt", DateTime.Now.ToString("yyyy-MM-dd-HH"));
+            else
+                fileName = string.Format("{0}_{1}.txt", fileName, DateTime.Now.ToString("yyyy-MM-dd-HH"));
+
+            path = string.Format("{0}/{1}", path, fileName);
+
+            if (!File.Exists(path))
+                using (var fs = File.Create(path)) { }
+
+            //写日志
+            using (var fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
             {
-                var path = string.Format("{0}/App_Data/log/{1}/{2}", AppDomain.CurrentDomain.BaseDirectory
-                                            , DateTime.Now.ToString("yyyy-MM"), DateTime.Now.ToString("yyyy-MM-dd"));
-                try
+                using (var m_streamWriter = new StreamWriter(fs))
                 {
-                    lockSlim.EnterWriteLock();
-
-                    //新建文件夹
-                    if (!Directory.Exists(path))
-                        Directory.CreateDirectory(path);
-
-                    if (fileName == "")
-                        fileName = string.Format("{0}.txt", DateTime.Now.ToString("yyyy-MM-dd-HH"));
-                    else
-                        fileName = string.Format("{0}_{1}.txt", fileName, DateTime.Now.ToString("yyyy-MM-dd-HH"));
-
-                    //写日志
-                    using (var fs = new FileStream(string.Format("{0}/{1}", path, fileName), FileMode.OpenOrCreate, FileAccess.Write))
-                    {
-                        using (var m_streamWriter = new StreamWriter(fs))
-                        {
-                            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
-                            m_streamWriter.WriteLine(string.Format("[{0}]{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logContent));
-                            m_streamWriter.WriteLine("");
-                            m_streamWriter.Flush();
-                            m_streamWriter.Close();
-                            fs.Close();
-                        }
-                    }
-                }
-                finally
-                {
-                    lockSlim.ExitWriteLock();
+                    m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+                    m_streamWriter.WriteLine(string.Format("[{0}]{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logContent));
+                    m_streamWriter.WriteLine("");
+                    m_streamWriter.Flush();
+                    m_streamWriter.Close();
+                    fs.Close();
                 }
             }
         }
