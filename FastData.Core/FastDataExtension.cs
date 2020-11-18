38738a6c691f8971c0655de9ca589ec8627ceb30
weizhonzhen <czdswzz@126.com>
2020-11-19 07:05:17 +0800

    Add files via upload

---
+++ FastData.Core/FastDataExtension.cs
@@ -0,0 +1,15 @@
+﻿using FastData.Core.Repository;
+using FastUntility.Core;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FastDataExtension
+    {
+        public static IServiceCollection AddFastData(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IFastRepository, FastRepository>();
+            ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
+            return serviceCollection;
+        }
+    }
+}
