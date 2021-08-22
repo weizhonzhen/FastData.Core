e3f76ea571e3941c19705ccca8ec34fb6323ef97
weizhonzhen <czdswzz@126.com>
2021-08-22 13:55:21 +0800

    Add files via upload

--- FastData.Core/Repository/FastRepository.cs
+++ FastData.Core/Repository/FastRepository.cs
@@ -1148,19 +1148,20 @@ namespace FastData.Core.Repository
             {
                 using (var tempDb = new DataContext(key))
                 {
-                    result = tempDb.ExecuteSql(sql, param, false);
                     config = tempDb.config;
+                    config.IsOutSql = config.IsOutSql ? config.IsOutSql : isOutSql;
+                    result = tempDb.ExecuteSql(sql, param, false, config.IsOutSql,false);
                 }
             }
             else
             {
-                result = db.ExecuteSql(sql, param, false);
                 config = db.config;
+                config.IsOutSql = config.IsOutSql ? config.IsOutSql : isOutSql;
+                result = db.ExecuteSql(sql, param,false,config.IsOutSql,false);
             }
 
             stopwatch.Stop();
 
-            config.IsOutSql = config.IsOutSql ? config.IsOutSql : isOutSql;
             DbLog.LogSql(config.IsOutSql, result.Sql, config.DbType, stopwatch.Elapsed.TotalMilliseconds);
 
             return result.writeReturn;
