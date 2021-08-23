a9c017d8a33751cc2065963d64ad9e5ddaeda1d8
weizhonzhen <czdswzz@126.com>
2021-08-23 16:13:09 +0800

    Add files via upload

--- FastData.Core/Aop/AopEnum.cs
+++ FastData.Core/Aop/AopEnum.cs
@@ -15,30 +15,29 @@ namespace FastData.Core.Aop
 
         Execute_Sql_Bool = 8,
         Execute_Sql_Model = 9,
-        Execute_Sql_Dic = 9,
+        Execute_Sql_Dic = 10,
 
 
-        Map_List_Model = 10,
-        Map_List_Dic = 10,
-        Map_Page_Dic = 10,
-        Map_Page_Model = 10,
-        Map_Write = 11,
+        Map_List_Model = 11,
+        Map_List_Dic = 12,
+        Map_Page_Dic = 13,
+        Map_Page_Model = 14,
+        Map_Write = 15,
 
 
-        Query_List_Lambda = 10,
-        Query_Dic_Lambda = 11,
-        Query_DataTable_Lambda = 12,
-        Query_Json_Lambda = 13,
-        Query_Count_Lambda = 14,
-        Query_Page_Lambda_Dic = 15,
-        Query_Page_Lambda_Model = 16,
-        Query_Page_Sql_Dic = 16,
-        Query_Page_Sql_Model = 16,
+        Query_List_Lambda = 16,
+        Query_Dic_Lambda = 17,
+        Query_DataTable_Lambda = 18,
+        Query_Json_Lambda = 19,
+        Query_Count_Lambda = 20,
+        Query_Page_Lambda_Dic = 21,
+        Query_Page_Lambda_Model = 22,
+        Query_Page_Sql_Dic = 23,
+        Query_Page_Sql_Model = 24,
 
-        DataContext = 11,
-        ParsingXml =11,
-        CodeFirst=11
-
+        DataContext = 25,
+        ParsingXml = 26,
+        CodeFirst = 27
 
     }
 }
