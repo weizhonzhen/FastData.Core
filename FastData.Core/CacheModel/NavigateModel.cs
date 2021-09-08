80773dfb9252ca944b38cbe7cb5025a73fdf42e8
weizhonzhen <czdswzz@126.com>
2021-09-08 15:59:24 +0800

    Add files via upload

---
+++ FastData.Core/CacheModel/NavigateModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FastData.Core.CacheModel
+{
+    internal class NavigateModel
+    {
+        public System.Type PropertyType { get; set; }
+
+        public string Name { get; set; }
+
+        public string Key { get; set; }
+
+        public bool IsList { get; set; }
+
+        public string Appand { get; set; }
+
+        public string MemberName { get; set; }
+
+        public System.Type MemberType { get; set; }
+    }
+}
