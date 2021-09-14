338a9d20ed9ca8913e01936531a16497c2225ec5
weizhonzhen <czdswzz@126.com>
2021-09-14 14:30:24 +0800

    Add files via upload

---
+++ FastData.Core/Aop/BaseAop.cs
@@ -0,0 +1,159 @@
+﻿using FastData.Core.Model;
+using FastUntility.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using System.Linq;
+
+namespace FastData.Core.Aop
+{
+    internal static class BaseAop
+    {
+        #region Aop Before
+        /// <summary>
+        /// Aop Before
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="name"></param>
+        /// <param name="param"></param>
+        /// <param name="config"></param>
+        public static void AopBefore(List<string> tableName, string sql, List<DbParameter> param, ConfigModel config, bool isRead, AopType type)
+        {
+            IFastAop aop = ServiceContext.Engine.Resolve<IFastAop>();
+            if (aop != null)
+            {
+                var context = new BeforeContext();
+
+                if (tableName != null)
+                    context.tableName = tableName;
+
+                context.sql = sql;
+
+                if (param != null)
+                    context.param = param;
+
+                context.dbType = config.DbType;
+                context.isRead = isRead;
+                context.isWrite = !isRead;
+                context.type = type;
+
+                aop.Before(context);
+            }
+        }
+        #endregion
+
+        #region Aop After
+        /// <summary>
+        /// Aop After
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="name"></param>
+        /// <param name="param"></param>
+        /// <param name="config"></param>
+        public static void AopAfter(List<string> tableName, string sql, List<DbParameter> param, ConfigModel config, bool isRead, AopType type, object result)
+        {
+            IFastAop aop = ServiceContext.Engine.Resolve<IFastAop>();
+            if (aop != null)
+            {
+                var context = new AfterContext();
+
+                if (tableName != null)
+                    context.tableName = tableName;
+
+                context.sql = sql;
+
+                if (param != null)
+                    context.param = param;
+
+                context.dbType = config.DbType;
+                context.isRead = isRead;
+                context.isWrite = !isRead;
+                context.result = result;
+                context.type = type;
+
+                aop.After(context);
+            }
+        }
+        #endregion
+
+        #region Aop Exception
+        /// <summary>
+        /// Aop Exception
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="name"></param>
+        public static void AopException(Exception ex, string name, AopType type, ConfigModel config)
+        {
+            IFastAop aop = ServiceContext.Engine.Resolve<IFastAop>();
+            if (aop != null)
+            {
+                var context = new ExceptionContext();
+                context.name = name;
+                context.type = type;
+                context.ex = ex;
+                context.dbType = config?.DbType;
+
+                aop.Exception(context);
+            }
+        }
+        #endregion
+
+        #region Aop Map Before
+        /// <summary>
+        /// Aop Map Before
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="name"></param>
+        /// <param name="param"></param>
+        /// <param name="config"></param>
+        public static void AopMapBefore(string mapName, string sql, DbParameter[] param, ConfigModel config, AopType type)
+        {
+            IFastAop aop = ServiceContext.Engine.Resolve<IFastAop>();
+            if (aop != null)
+            {
+                var context = new MapBeforeContext();
+                context.mapName = mapName;
+                context.sql = sql;
+                context.type = type;
+
+                if (param != null)
+                    context.param = param.ToList();
+
+                context.dbType = config.DbType;
+
+                aop.MapBefore(context);
+            }
+        }
+        #endregion
+
+        #region Aop Map After
+        /// <summary>
+        /// Aop Map After
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="name"></param>
+        /// <param name="param"></param>
+        /// <param name="config"></param>
+        public static void AopMapAfter(string mapName, string sql, DbParameter[] param, ConfigModel config, AopType type, object data)
+        {
+            IFastAop aop = ServiceContext.Engine.Resolve<IFastAop>();
+            if (aop != null)
+            {
+                var context = new MapAfterContext();
+                context.mapName = mapName;
+                context.sql = sql;
+                context.type = type;
+
+                if (param != null)
+                    context.param = param.ToList();
+
+                context.dbType = config.DbType;
+                context.result = data;
+
+                aop.MapAfter(context);
+            }
+        }
+        #endregion
+    }
+}
