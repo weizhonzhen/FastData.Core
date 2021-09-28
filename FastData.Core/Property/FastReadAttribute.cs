6a7248473f80fdaf1b6b74c58647c34218fda11f
weizhonzhen <czdswzz@126.com>
2021-09-28 11:35:18 +0800

    Add files via upload

---
+++ FastData.Core/Property/FastReadAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastData.Core.Property
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FastReadAttribute : Attribute
+    {
+        public string sql { get; set; }
+
+        public string dbKey { get; set; }
+    }
+}
---
+++ FastData.Core/Property/FastWriteAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastData.Core.Property
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FastWriteAttribute : Attribute
+    {
+        public string sql { get; set; }
+
+        public string dbKey { get; set; }
+    }
+}
