8e4b0ed61c1ff1a5b23ddabd1a7cb27ea7fc6a73
weizhonzhen <czdswzz@126.com>
2021-09-28 11:34:30 +0800

    Add files via upload

---
+++ FastData.Core/CacheModel/ServiceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FastData.Core.CacheModel
+{
+    internal class ServiceModel
+    {
+        public Dictionary<string, object> param { get; set; } = new Dictionary<string, object>();
+
+        public string sql { get; set; }
+
+        public bool isWrite { get; set; }
+
+        public bool isList { get; set; }
+
+        public System.Type type { get; set; }
+
+        public String dbKey { get; set; }
+    }
+}
