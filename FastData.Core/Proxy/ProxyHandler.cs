48547e5c036d1504d89a26dfe9eb11681b60359d
weizhonzhen <czdswzz@126.com>
2021-10-07 22:43:56 +0800

    Add files via upload

--- FastData.Core/Proxy/ProxyHandler.cs
+++ FastData.Core/Proxy/ProxyHandler.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using System.Linq;
 using FastData.Core.Property;
 using FastData.Core.Model;
+using FastUntility.Core.Page;
 
 namespace FastData.Core.Proxy
 {
@@ -16,6 +17,7 @@ namespace FastData.Core.Proxy
     {
         public object Invoke(object proxy, MethodInfo method, object[] args)
         {
+            var pModel = new PageModel();
             var param = new List<DbParameter>();
             var config = DataConfig.Get();
             var key = string.Format("{0}.{1}", method.DeclaringType.Name, method.Name);
@@ -23,26 +25,60 @@ namespace FastData.Core.Proxy
             if (DbCache.Exists(config.CacheType, key))
             {
                 var model = DbCache.Get<ServiceModel>(config.CacheType, key);
-                config = DataConfig.Get(model.dbKey);
 
+                if (model.isPage)
+                    pModel = (PageModel)args.ToList().Find(a => a.GetType() == typeof(PageModel));
+
+                config = DataConfig.Get(model.dbKey);
                 if (model.isSysType)
                 {
                     for (int i = 0; i < args.Length; i++)
                     {
+                        if (args[i].GetType() == typeof(PageModel))                        
+                            continue;
+                        
                         var temp = DbProviderFactories.GetFactory(config).CreateParameter();
                         temp.ParameterName = model.param.GetValue(i.ToString()).ToStr();
                         temp.Value = args[i];
                         param.Add(temp);
                     }
                 }
+                else if (model.isDic)
+                {
+                    if (!args.ToList().Exists(a => a.GetType() == typeof(Dictionary<string, object>)))
+                        throw new Exception($"error: service {method.DeclaringType.Name} , method {method.Name} , param type {args[0].GetType().Name} is not support");
+
+                    var dic = (Dictionary < string, object>)args.ToList().Find(a => a.GetType() == typeof(Dictionary<string, object>));
+                    var tempDic = new Dictionary<int, string>();
+
+                    foreach (KeyValuePair<string, object> keyValue in dic)
+                    {
+                        key = string.Format("{0}{1}", config.Flag, keyValue.Key).ToLower();
+                        if (model.sql.IndexOf(key) > 0)
+                        {
+                            tempDic.Add(model.sql.IndexOf(key), keyValue.Key);
+                        }
+                    }
+                    var list = tempDic.OrderBy(d => d.Key).ToList();
+                    foreach (KeyValuePair<int, string> keyValue in list)
+                    {
+                        var temp = DbProviderFactories.GetFactory(config).CreateParameter();
+                        temp.ParameterName = keyValue.Value;
+                        temp.Value = dic.GetValue(keyValue.Value);
+                        param.Add(temp);
+                    }
+                }
                 else
                 {
-                    var dyn = new DynamicGet(Activator.CreateInstance(method.GetParameters()[0].ParameterType));
+                    var data = args.ToList().Find(a => a.GetType() != typeof(PageModel));
+                    var type = method.GetParameters().ToList().Find(a => a.ParameterType != typeof(PageModel)).ParameterType;
+                    var dyn = new DynamicGet(Activator.CreateInstance(type));
+
                     for (int i = 0; i < model.param.Count; i++)
                     {
                         var temp = DbProviderFactories.GetFactory(config).CreateParameter();
                         temp.ParameterName = model.param.GetValue(i.ToString()).ToStr();
-                        temp.Value = dyn.GetValue(args[0], temp.ParameterName,true);
+                        temp.Value = dyn.GetValue(data, temp.ParameterName);
                         temp.ParameterName = temp.ParameterName.ToLower();
                         param.Add(temp);
                     }
@@ -52,8 +88,8 @@ namespace FastData.Core.Proxy
                 {
                     if (model.isWrite)
                         return db.ExecuteSql(model.sql, param.ToArray(), Aop.AopType.FaseWrite).writeReturn;
-                    else                    
-                        return db.FastReadAttribute(model, param); 
+                    else
+                        return db.FastReadAttribute(model, param, pModel);
                 }
             }
 
