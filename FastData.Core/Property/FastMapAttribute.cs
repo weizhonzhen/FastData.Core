e2fc45f317c45ead48ba5c8f61638fd678960167
weizhonzhen <czdswzz@126.com>
2021-10-12 15:41:12 +0800

    Add files via upload

---
+++ FastData.Core/Property/FastMapAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastData.Core.Property
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FastMapAttribute : Attribute
+    {
+        public string xml { get; set; }
+
+        public string dbKey { get; set; }
+
+        public bool isPage { get; set; }
+    }
+}
