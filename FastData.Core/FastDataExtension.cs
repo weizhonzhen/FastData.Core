bf2629cb3eab56d9baa3c96bffa97f539ba7f843
weizhonzhen <czdswzz@126.com>
2021-10-20 15:08:03 +0800

    Add files via upload

--- FastData.Core/FastDataExtension.cs
+++ FastData.Core/FastDataExtension.cs
@@ -89,25 +89,4 @@ namespace Microsoft.Extensions.DependencyInjection
             return serviceCollection;
         }
     }
-
-    public class ConfigData
-    {
-        public bool IsResource { get; set; }
-
-        public bool IsCodeFirst { get; set; }
-
-        public string NamespaceCodeFirst { get; set; }
-
-        public string NamespaceProperties { get; set; }
-
-        public string dbKey { get; set; }
-
-        public string dbFile { get; set; } = "db.json";
-
-        public string mapFile { get; set; } = "map.json";
-
-        public string NamespaceService { get; set; }
-
-        public IFastAop aop { get; set; }
-    }
 }
