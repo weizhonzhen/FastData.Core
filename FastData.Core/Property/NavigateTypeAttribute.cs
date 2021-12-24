cde12ae0a430b43d804b209c993c64ba30e32719
weizhonzhen <czdswzz@126.com>
2021-12-24 11:58:36 +0800

    Add files via upload

--- FastData.Core/Property/NavigateTypeAttribute.cs
+++ FastData.Core/Property/NavigateTypeAttribute.cs
@@ -12,5 +12,20 @@ namespace FastData.Core.Property
         /// 类型
         /// </summary>
         public System.Type Type { get; set; }
+
+        /// <summary>
+        ///  否增加
+        /// </summary>
+        public bool IsAdd { get; set; }
+
+        /// <summary>
+        /// 是否修改
+        /// </summary>
+        public bool IsUpdate { get; set; }
+
+        /// <summary>
+        /// 是否删除
+        /// </summary>
+        public bool IsDel { get; set; }
     }
 }
