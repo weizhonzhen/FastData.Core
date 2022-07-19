94b7a36f03951171bc08b925bfff7814fc0c5abc
weizhonzhen <czdswzz@126.com>
2022-07-19 13:01:57 +0800

    Add files via upload

--- FastData.Core/Proxy/ProxyHandler.cs
+++ FastData.Core/Proxy/ProxyHandler.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using FastData.Core.Property;
 using FastData.Core.Model;
 using FastUntility.Core.Page;
+using DbProviderFactories = FastData.Core.Base.DbProviderFactories;
 
 namespace FastData.Core.Proxy
 {
