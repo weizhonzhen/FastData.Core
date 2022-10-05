3cb8eb349589b83402022a6ebae3db73f3b6c242
weizhonzhen <czdswzz@126.com>
2022-10-05 21:53:18 +0800

    Add files via upload

--- FastData.Core/Exception/ProxyException.cs
+++ FastData.Core/Exception/ProxyException.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace FastData.Core
+{
+    internal class ProxyException : Exception
+    {
+        public ProxyException(string message) : base(message)
+        {
+
+        }
+    }
+}
