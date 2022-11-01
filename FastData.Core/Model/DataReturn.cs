dece6294e6ff4b7a4929599cf176affa6c99586b
weizhonzhen <czdswzz@126.com>
2022-11-01 08:32:27 +0800

    Add files via upload

--- FastData.Core/Model/DataReturn.cs
+++ FastData.Core/Model/DataReturn.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Data;
 using FastUntility.Core.Page;
+using NPOI.SS.Formula.Functions;
 
 namespace FastData.Core.Model
 {
     /// <summary>
     /// 返回操作数据结果
     /// </summary>
-    public sealed class DataReturn<T> where T : class,new()
+    public class DataReturn<T> where T : class, new()
     {
         /// <summary>
         /// 条数
@@ -40,7 +41,43 @@ namespace FastData.Core.Model
         public WriteReturn WriteReturn { set; get; } = new WriteReturn();
     }
 
-     /// <summary>
+    /// <summary>
+    /// 返回动态类型
+    /// </summary>
+    public class DataReturnDyn
+    {
+        /// <summary>
+        /// 条数
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// 实体
+        /// </summary>
+        public dynamic Item { set; get; }
+
+        /// <summary>
+        /// 列表
+        /// </summary>
+        public List<dynamic> List { set; get; }=new List<dynamic>();
+
+        /// <summary>
+        /// sql
+        /// </summary>
+        public string Sql { get; set; }
+
+        /// <summary>
+        /// 分页
+        /// </summary>
+        public PageResultDyn PageResult { set; get; } = new PageResultDyn();
+
+        /// <summary>
+        /// 写返回结果
+        /// </summary>
+        public dynamic WriteReturn { set; get; }
+    }
+
+    /// <summary>
     /// 返回操作数据结果
     /// </summary>
     public class DataReturn
@@ -49,7 +86,7 @@ namespace FastData.Core.Model
         /// 条数
         /// </summary>
         public int Count { get; set; }
-        
+
         /// <summary>
         /// sql
         /// </summary>
