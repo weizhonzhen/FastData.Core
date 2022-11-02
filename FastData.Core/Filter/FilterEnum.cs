9595c1f9ffb24dbd305f43f59bec43c40c4b118c
weizhonzhen <czdswzz@126.com>
2022-11-02 22:29:41 +0800

    Add files via upload

--- FastData.Core/Filter/FilterEnum.cs
+++ FastData.Core/Filter/FilterEnum.cs
@@ -16,6 +16,8 @@ namespace FastData.Core.Filter
         Query_Page_Lambda_Model = 8,
         Query_Page_Sql_Model = 9,
         Query_Page_Sql_Dic = 10,
-        FastRead_Page =11
+        FastRead_Page =11,
+        Query_Dyn_Lambda=12,
+        Query_Page_Lambda_Dyn = 13,
     }
 }
